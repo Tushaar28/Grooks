47776dd418f3428e47fcd6a3eaf99dc45be36f34
Tushaar Tiwari <tushaartiwari1997@gmail.com>
2021-12-11 22:45:40 +0530

    defined folder structure

---
+++ lib/constants/constants.dart
@@ -0,0 +1,26 @@
+// ignore: constant_identifier_names
+const String APP_NAME = "Grooks";
+// ignore: constant_identifier_names
+const String LOGO_PATH = "assets/images/logo.png";
+// ignore: constant_identifier_names
+const String USERS_COLLECTION = "users";
+// ignore: constant_identifier_names
+const String CATEGORIES_COLLECTION = "categories";
+// ignore: constant_identifier_names
+const String FEEDBACK_CATEGORIES_COLLECTION = "feedback_categories";
+// ignore: constant_identifier_names
+const String FEEDBACK_COLLECTION = "feedbacks";
+// ignore: constant_identifier_names
+const String PARAMETERS_COLLECTION = "parameters";
+// ignore: constant_identifier_names
+const String STORES_COLLECTION = "store";
+// ignore: constant_identifier_names
+const String SETTINGS_COLLECTION = "settings";
+// ignore: constant_identifier_names
+const String QUESTIONS_COLLECTION = "questions";
+// ignore: constant_identifier_names
+const String TRADES_COLLECTION = "trades";
+// ignore: constant_identifier_names
+const String WALLETS_COLLECTION = "wallets";
+// ignore: constant_identifier_names
+const String MY_HOURS_COLLECTION = "myHours";
