7f818538e0069a5df43039276a33a45b0612c6ed
Tushaar Tiwari <tushaartiwari1997@gmail.com>
2022-01-08 14:06:56 +0530

    added custom dynamic link parameters and removed google login

--- lib/screens/authentication/login_screen.dart
+++ lib/screens/authentication/login_screen.dart
@@ -1,13 +1,9 @@
 import 'package:auto_size_text/auto_size_text.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:grooks_dev/models/question.dart';
-import 'package:grooks_dev/models/user.dart';
 import 'package:grooks_dev/resources/firebase_repository.dart';
 import 'package:grooks_dev/screens/authentication/otp_input_screen.dart';
-import 'package:grooks_dev/screens/user/navbar_screen.dart';
 
 class LoginScreen extends StatefulWidget {
   final String? referralCode;
@@ -151,105 +147,6 @@ class _LoginScreenState extends State<LoginScreen> {
                     ),
                     SizedBox(
                       height: MediaQuery.of(context).size.height * 0.03,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Container(
-                          color: Colors.grey,
-                          height: 1,
-                          width: MediaQuery.of(context).size.width * 0.42,
-                        ),
-                        SizedBox(
-                          width: MediaQuery.of(context).size.height * 0.03,
-                          child: const Center(
-                            child: Text(
-                              "OR",
-                              style: TextStyle(
-                                color: Colors.grey,
-                                fontSize: 12,
-                              ),
-                            ),
-                          ),
-                        ),
-                        Container(
-                          color: Colors.grey,
-                          height: 1,
-                          width: MediaQuery.of(context).size.width * 0.415,
-                        ),
-                      ],
-                    ),
-                    SizedBox(
-                      height: MediaQuery.of(context).size.height * 0.03,
-                    ),
-                    Row(
-                      mainAxisSize: MainAxisSize.max,
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        InkWell(
-                          child: Container(
-                            height: MediaQuery.of(context).size.height * 0.07,
-                            width: MediaQuery.of(context).size.width * 0.07,
-                            color: Colors.transparent,
-                            child: SvgPicture.asset(
-                              "assets/images/google.svg",
-                              fit: BoxFit.contain,
-                            ),
-                          ),
-                          onTap: () async {
-                            try {
-                              UserCredential user =
-                                  await _repository.signInWithGoogle();
-                              Users? userDetails =
-                                  await _repository.getUserDetails();
-                              if (userDetails != null &&
-                                  userDetails.isActive == false) {
-                                await _repository.signOut();
-                                ScaffoldMessenger.of(context)
-                                    .hideCurrentSnackBar();
-                                ScaffoldMessenger.of(context).showSnackBar(
-                                  const SnackBar(
-                                    content: Text(
-                                      "Your account has been suspended",
-                                      style: TextStyle(
-                                        color: Colors.white,
-                                      ),
-                                    ),
-                                    backgroundColor: Colors.red,
-                                    duration: Duration(seconds: 2),
-                                  ),
-                                );
-                                return;
-                              }
-                              if (userDetails == null ||
-                                  user.additionalUserInfo!.isNewUser) {
-                                await _repository.addUser(
-                                  name: user.user!.displayName!,
-                                  uid: user.user!.uid,
-                                  mobile: user.user?.phoneNumber,
-                                  profileUrl: user.user?.photoURL,
-                                  email: user.user!.email!,
-                                );
-                              }
-                              if (userDetails != null) {
-                                userDetails =
-                                    await _repository.getUserDetails();
-                              }
-                              Navigator.pushAndRemoveUntil(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) => NavbarScreen(
-                                    user: userDetails,
-                                    initialPage: 'Home',
-                                  ),
-                                ),
-                                (Route<dynamic> route) => false,
-                              );
-                            } catch (error) {}
-                          },
-                        ),
-                      ],
                     ),
                   ],
                 ),
