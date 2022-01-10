9b6963bfe6a1168dea823c782f844e4ef35cc3c5
Tushaar Tiwari <tushaartiwari1997@gmail.com>
2022-01-11 00:08:49 +0530

    added how to trade screen

--- lib/widgets/how_to_trade_carousel.dart
+++ lib/widgets/how_to_trade_carousel.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:grooks_dev/models/user.dart';
+import 'package:grooks_dev/widgets/custom_button.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 class HowToTradeCarousel extends StatefulWidget {
   final Users user;
@@ -13,92 +15,45 @@ class HowToTradeCarousel extends StatefulWidget {
 }
 
 class _HowToTradeCarouselState extends State<HowToTradeCarousel> {
-  late final List<Map<String, dynamic>> _pages;
+  late final List<String> _pages;
   late int _index;
 
   @override
   void initState() {
     super.initState();
     _pages = [
-      {
-        "image": "assets/images/how_to_trade_fg.png",
-        "text": "hiuewhfiwehfwhfgwbnfiubwfuwfewfwgwrgw",
-        "button": "assets/images/how_to_trade_button_next.png",
-      },
-      {
-        "image": "assets/images/how_to_trade_fg.png",
-        "text": "iukuyjktjukyktuktuktuktkutkutukutktukuk",
-        "button": "assets/images/how_to_trade_button1.png",
-      },
-      {
-        "image": "assets/images/how_to_trade_fg.png",
-        "text": "hiuewhfiwehfwhfgwbnfiubwfuwfewfwgwrgw",
-        "button": "assets/images/how_to_trade_button2.png",
-      },
-      {
-        "image": "assets/images/how_to_trade_fg.png",
-        "text": "iukuyjktjukyktuktuktuktkutkutukutktukuk",
-        "button": "assets/images/how_to_trade_button3.png",
-      },
-      {
-        "image": "assets/images/how_to_trade_fg.png",
-        "text": "iukuyjktjukyktuktuktuktkutkutukutktukuk",
-        "button": "assets/images/how_to_trade_button_finished.png",
-      },
+      "assets/images/Trade1.jpg",
+      "assets/images/Trade2.jpg",
+      "assets/images/Trade3.jpg",
+      "assets/images/Trade4.jpg",
+      "assets/images/Trade5.jpg",
     ];
     _index = 0;
   }
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Column(
-          children: [
-            SizedBox(height: MediaQuery.of(context).size.height * 0.2),
-            Padding(
-              padding: EdgeInsets.symmetric(
-                  horizontal: MediaQuery.of(context).size.width * 0.075),
-              child: Container(
-                height: MediaQuery.of(context).size.height * 0.4,
-                width: double.infinity,
-                decoration: const BoxDecoration(
-                  color: Color(0x25007AFF),
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(20),
-                  ),
-                ),
-                child: Padding(
-                  padding: EdgeInsets.fromLTRB(
-                    MediaQuery.of(context).size.width * 0.04,
-                    MediaQuery.of(context).size.height * 0.19,
-                    MediaQuery.of(context).size.width * 0.04,
-                    MediaQuery.of(context).size.height * 0.01,
-                  ),
-                  child: Text(
-                    _pages[_index]["text"],
-                  ),
+    return Container(
+      color: const Color(0xFF1C3857),
+      height: MediaQuery.of(context).size.height,
+      width: double.infinity,
+      child: CarouselSlider(
+        options: CarouselOptions(
+          autoPlay: true,
+          viewportFraction: 1,
+          height: MediaQuery.of(context).size.height * 0.85,
+        ),
+        items: _pages
+            .map(
+              (image) => SizedBox(
+                child: Image.asset(
+                  image,
+                  fit: BoxFit.contain,
                 ),
               ),
-            ),
-            SizedBox(
-              height: MediaQuery.of(context).size.height * 0.1,
-            ),
-            SizedBox(
-              height: MediaQuery.of(context).size.height * 0.125,
-              child: InkWell(
-                child: Image.asset(_pages[_index]["button"]),
-                onTap: () =>
-                    setState(() => _index = (_index + 1) % _pages.length),
-              ),
-            ),
-          ],
-        ),
-        Padding(
-          padding: EdgeInsets.zero,
-          child: Image.asset(_pages[_index]["image"]),
-        ),
-      ],
+            )
+            .toList(),
+      ),
     );
   }
 }
