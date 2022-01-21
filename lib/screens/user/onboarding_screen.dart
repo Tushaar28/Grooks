baeb9d596a2e97f2af1b6e716e7bc013f4265c39
Tushaar Tiwari <tushaartiwari1997@gmail.com>
2022-01-21 16:29:22 +0530

    fixed bugs

--- lib/screens/user/onboarding_screen.dart
+++ lib/screens/user/onboarding_screen.dart
@@ -66,7 +66,6 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
                     ),
                     text: "Get Started",
                     elevation: 80,
-                    color: const Color(0x801C3857),
                     textStyle: const TextStyle(
                       color: Colors.white,
                       fontSize: 18,
