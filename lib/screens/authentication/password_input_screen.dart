a400e1bac22720242fce69a58f24b89f7cf82146
Tushaar Tiwari <tushaartiwari1997@gmail.com>
2022-03-04 22:38:35 +0530

    added password option for users

--- lib/screens/authentication/password_input_screen.dart
+++ lib/screens/authentication/password_input_screen.dart
@@ -2,7 +2,11 @@ import 'package:auto_size_text/auto_size_text.dart';
 import 'package:cloud_functions/cloud_functions.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:grooks_dev/models/question.dart';
+import 'package:grooks_dev/models/user.dart';
 import 'package:grooks_dev/resources/firebase_repository.dart';
+import 'package:grooks_dev/screens/user/navbar_screen.dart';
+import 'package:grooks_dev/screens/user/user_detail_screen.dart';
 import 'package:otp_text_field/otp_field.dart';
 import 'package:otp_text_field/otp_field_style.dart';
 import 'package:otp_text_field/style.dart';
@@ -10,10 +14,14 @@ import 'package:otp_text_field/style.dart';
 class PasswordScreen extends StatefulWidget {
   final String mobile;
   final String? referralCode;
+  final Map<String, dynamic>? sharedViewMap;
+  final Question? question;
   const PasswordScreen({
     Key? key,
     required this.mobile,
     this.referralCode,
+    this.question,
+    this.sharedViewMap,
   }) : super(key: key);
 
   @override
@@ -26,6 +34,11 @@ class _PasswordScreenState extends State<PasswordScreen> {
   late final FirebaseRepository _repository;
   late bool _isLoading;
   late final GlobalKey<ScaffoldState> _scaffoldKey;
+  final suspendedAccountSnackbar = const SnackBar(
+    content: AutoSizeText('Your account has been suspended.'),
+    backgroundColor: Colors.red,
+    duration: Duration(seconds: 2),
+  );
 
   @override
   void initState() {
@@ -176,80 +189,80 @@ class _PasswordScreenState extends State<PasswordScreen> {
                                         "password":
                                             _passwordController.text.trim(),
                                       });
-                                      print("RESULT = $result");
-                                      // setState(() => _isLoading = true);
-                                      // UserCredential credential =
-                                      //     await onSubmitted(context);
+                                      String token = result.data;
+                                      setState(() => _isLoading = true);
+                                      UserCredential credential =
+                                          await _firebaseAuth
+                                              .signInWithCustomToken(token);
+                                      Users? userDetails =
+                                          await _repository.getUserDetails();
+                                      if (userDetails != null) {
+                                        if (userDetails.isActive == false) {
+                                          setState(() => _isLoading = false);
+                                          ScaffoldMessenger.of(context)
+                                              .hideCurrentSnackBar();
+                                          ScaffoldMessenger.of(context)
+                                              .showSnackBar(
+                                                  suspendedAccountSnackbar);
+                                          return;
+                                        }
+                                      }
 
-                                      // Users? userDetails =
-                                      //     await _repository.getUserDetails();
-                                      // if (userDetails != null) {
-                                      //   if (userDetails.isActive == false) {
-                                      //     setState(() => _isLoading = false);
-                                      //     ScaffoldMessenger.of(context)
-                                      //         .hideCurrentSnackBar();
-                                      //     ScaffoldMessenger.of(context)
-                                      //         .showSnackBar(
-                                      //             suspendedAccountSnackbar);
-                                      //     return;
-                                      //   }
-                                      // }
-
-                                      // if (credential
-                                      //         .additionalUserInfo!.isNewUser ||
-                                      //     userDetails == null) {
-                                      //   Navigator.pushAndRemoveUntil(
-                                      //       context,
-                                      //       MaterialPageRoute(
-                                      //         builder: (context) =>
-                                      //             UserDetailScreen(
-                                      //           user: credential.user!,
-                                      //           referralCode:
-                                      //               widget.referralCode,
-                                      //           sharedViewMap:
-                                      //               widget.sharedViewMap,
-                                      //           question: widget.question,
-                                      //         ),
-                                      //       ),
-                                      //       (Route<dynamic> route) => false);
-                                      // } else {
-                                      //   Users? userDetails =
-                                      //       await _repository.getUserDetails();
-                                      //   await _repository.updateUser(
-                                      //     userId: userDetails!.id,
-                                      //     data: {'lastLoginAt': DateTime.now()},
-                                      //   );
-                                      //   if (widget.sharedViewMap != null &&
-                                      //       widget.question != null) {
-                                      //     // Navigator.pushAndRemoveUntil(
-                                      //     //     context,
-                                      //     //     MaterialPageRoute(
-                                      //     //       builder: (context) =>
-                                      //     //           QuestionDetailWidget(
-                                      //     //         user: userDetails,
-                                      //     //         questionId:
-                                      //     //             widget.question!.id,
-                                      //     //         questionName:
-                                      //     //             widget.question!.name,
-                                      //     //         sharedViewMap:
-                                      //     //             widget.sharedViewMap,
-                                      //     //       ),
-                                      //     //     ),
-                                      //     //     (route) => false);
-                                      //   } else {
-                                      //     Navigator.pushAndRemoveUntil(
-                                      //       context,
-                                      //       MaterialPageRoute(
-                                      //         builder: (context) =>
-                                      //             NavbarScreen(
-                                      //           user: userDetails,
-                                      //           initialPage: 'Home',
-                                      //         ),
-                                      //       ),
-                                      //       (Route<dynamic> route) => false,
-                                      //     );
-                                      //   }
-                                      // }
+                                      if (credential
+                                              .additionalUserInfo!.isNewUser ||
+                                          userDetails == null) {
+                                        Navigator.pushAndRemoveUntil(
+                                            context,
+                                            MaterialPageRoute(
+                                              builder: (context) =>
+                                                  UserDetailScreen(
+                                                user: credential.user!,
+                                                referralCode:
+                                                    widget.referralCode,
+                                                sharedViewMap:
+                                                    widget.sharedViewMap,
+                                                question: widget.question,
+                                              ),
+                                            ),
+                                            (Route<dynamic> route) => false);
+                                      } else {
+                                        Users? userDetails =
+                                            await _repository.getUserDetails();
+                                        await _repository.updateUser(
+                                          userId: userDetails!.id,
+                                          data: {'lastLoginAt': DateTime.now()},
+                                        );
+                                        if (widget.sharedViewMap != null &&
+                                            widget.question != null) {
+                                          // Navigator.pushAndRemoveUntil(
+                                          //     context,
+                                          //     MaterialPageRoute(
+                                          //       builder: (context) =>
+                                          //           QuestionDetailWidget(
+                                          //         user: userDetails,
+                                          //         questionId:
+                                          //             widget.question!.id,
+                                          //         questionName:
+                                          //             widget.question!.name,
+                                          //         sharedViewMap:
+                                          //             widget.sharedViewMap,
+                                          //       ),
+                                          //     ),
+                                          //     (route) => false);
+                                        } else {
+                                          Navigator.pushAndRemoveUntil(
+                                            context,
+                                            MaterialPageRoute(
+                                              builder: (context) =>
+                                                  NavbarScreen(
+                                                user: userDetails,
+                                                initialPage: 'Home',
+                                              ),
+                                            ),
+                                            (Route<dynamic> route) => false,
+                                          );
+                                        }
+                                      }
                                     } catch (error) {
                                       print("ERROR = $error");
                                       // setState(() => _isLoading = false);
